47c012259b6bfbb6435f19c12b6d872d4c9d9f4e
JUANECL314 <a01198418@tec.mx>
2025-12-04 20:38:09 -0600

    Cambios

--- Assets/Scripts/Level1Puzzle2/MazeStateMachine.cs
+++ Assets/Scripts/Level1Puzzle2/MazeStateMachine.cs
@@ -51,7 +51,7 @@ public class MazeStateMachine : MonoBehaviourPunCallbacks
 
         RestartUI();
         panelMagicLeft.SetActive(false);
-        enemy.SetActive(false);
+       
         //StateMachineStatus(MazeEnumState.Create);
     }
 
